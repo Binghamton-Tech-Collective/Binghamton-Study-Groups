880b8d3e8c8a16d768a8628678d38287bad9e121
Michael Fehrer <mfehrer@redhat.com>
2023-12-25 11:14:04 -0500

    added arrow button

--- src/app/home.js
+++ src/app/home.js
@@ -1,11 +1,15 @@
-import {View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import ArrowButton from '../components/functional/ArrowButton'
+
+
 const home = () => {
- return (
-   <View>
-    <Text>
-      Hello World
-    </Text>
-   </View>
- );
+  return (
+    <View>
+      <Text>
+        Hello World
+      </Text>
+      <ArrowButton text="arrow button text" />
+    </View>
+  );
 };
 export default home;
